409c2754a942455e600996a733ee1f1f2b081add
shrimp1box <shrimp1box@gmail.com>
2019-02-27 13:47:37 +0900

    Add global colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,38 @@
+// coralPink
+export const coralPink_05 = '#fff4f4';
+export const coralPink_10 = '#ffcdcf';
+export const coralPink_20 = '#ffacaf';
+export const coralPink_30 = '#ff9094';
+export const coralPink_40 = '#ff7a7e';
+export const coralPink_50 = '#ff6a6e';
+export const coralPink_70 = '#ff4c52';
+export const coralPink_90 = '#ff2e36';
+
+// coralViolet
+export const coralViolet_50 = '#e65f7b';
+export const coralViolet_60 = '#cc5583';
+export const coralViolet_70 = '#b34a85';
+export const coralViolet_80 = '#994082';
+export const coralViolet_90 = '#80357a';
+
+// gray
+export const gray_05 = '#f9fafa';
+export const gray_07 = '#f1f2f3';
+export const gray_10 = '#e3e6e8';
+export const gray_20 = '#cdd3d7';
+export const gray_30 = '#b8c0c4';
+export const gray_40 = '#a3adb2';
+export const gray_50 = '#8f999f';
+export const gray_60 = '#7b868c';
+export const gray_70 = '#677278';
+export const gray_80 = '#586166';
+export const gray_85 = '#485054';
+export const gray_90 = '#393f42';
+export const gray_95 = '#292e30';
+
+// etc
+export const blueGreen = '#409599';
+export const hulkGreen = '#4ab367';
+export const indigo = '#354380';
+export const mustard = '#f1c33f';
+export const shrekGreen = '#90cc55';
