7cbf0f5f318495104b3c00acd5b6d9ac0117ae43
shrimp1box <shrimp1box@gmail.com>
2019-02-27 22:00:46 +0900

    Update React version and Add default structure

---
+++ src/components/Post/Header/index.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */ jsx;
+import { jsx } from '@emotion/core';
+
+interface headerProps {
+  userName: string;
+  userProfileImage: string;
+  modifiedDate: string;
+}
+
+export const Header: React.FunctionComponent = () => {
+  return <h2 />;
+};
