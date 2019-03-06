3d8a790d7b9792d9b2682e00f7b52d91f7425d88
shrimp1box <shrimp1box@gmail.com>
2019-03-06 13:37:40 +0900

    Add return home button to GNB

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -2,12 +2,17 @@
 import { jsx } from '@emotion/core';
 import * as React from 'react';
 import * as styles from './styles';
+import Link from 'next/link';
 
 type GNBProps = {};
 
 export const GNB: React.FunctionComponent<GNBProps> = () => (
   <div css={styles.wrapper}>
-    <div css={styles.titleStyle}>Booklet</div>
+    <div css={styles.titleStyle}>
+      <Link href="/">
+        <a>Booklet</a>
+      </Link>
+    </div>
     <img css={styles.profileImageStyle} src="https://source.unsplash.com/random/100x100" />
   </div>
 );
