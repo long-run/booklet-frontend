d80c028e3ef292aa7dc0bc9e22f4bb920144efc1
Stacy Kim <imstacyyya@gmail.com>
2019-04-03 22:14:11 +0900

    modify GNB style

--- src/components/GNB/styles.ts
+++ src/components/GNB/styles.ts
@@ -4,18 +4,42 @@ import { colors } from '../../styles/colors';
 export const wrapper = css`
   display: flex;
   height: 24px;
-  padding: 10px 12px;
+  padding: 18px 20px;
   background-color: ${colors.coralPink_50};
   flex-direction: row;
 `;
 
-export const logoStyle = css`
+export const symbolStyle = css`
   display: inline-block;
   width: 20px;
   height: 24px;
   color: white;
   cursor: pointer;
   fill: white;
+`;
+
+export const logoStyle = css`
+  display: inline-block;
+  padding-left: 10px;
+  margin-bottom: 5px;
+  height: 14px;
+  color: white;
+  cursor: pointer;
+  fill: white;
+`;
+
+export const logoWrapper = css`
+  display: inline-block;
+  margin-left: 10px;
+  &::before {
+    position: absolute;
+    top: 24px;
+    width: 1px;
+    height: 12px;
+    opacity: 0.5;
+    background: white;
+    content: '';
+  }
 `;
 
 export const titleStyle = css`
@@ -27,8 +51,9 @@ export const titleStyle = css`
 
 export const profileImageStyle = css`
   display: inline-block;
-  width: 24px;
-  height: 24px;
+  margin-top: -2px;
+  width: 28px;
+  height: 28px;
   color: white;
   border-radius: 100px;
   cursor: pointer;
