f51c2bc0a3722f0868599658c2e240a1edb093a2
최아연 <ayon.c@ridi.com>
2019-05-14 05:12:40 +0900

    change thumbs up action

--- src/components/Post/Action/index.tsx
+++ src/components/Post/Action/index.tsx
@@ -2,21 +2,43 @@
 import { jsx } from '@emotion/core';
 import LikeIcon from '../../SVG/Like.svg';
 import * as styles from './styles';
+import React from 'react';
 
 export interface ActionProps {
   liked: boolean;
   likeCount: number;
 }
 
-export const Action: React.FunctionComponent<ActionProps> = (props: ActionProps) => {
-  const { liked, likeCount } = props;
-  console.log(liked);
-  return (
-    <div css={styles.actionWrapper}>
-      <button type="button" css={styles.likeButton}>
-        <LikeIcon css={styles.likeIcon} />
-        {likeCount} Likes
-      </button>
-    </div>
-  );
-};
+export class Action extends React.Component<ActionProps> {
+  state = {
+    hover: false,
+    liked: this.props.liked,
+    likeCount: this.props.likeCount,
+  };
+  setHover = (hover: boolean) => this.setState({ hover });
+  toggleLiked = () =>
+    this.setState({
+      liked: !this.state.liked,
+      likeCount: this.state.liked ? this.state.likeCount - 1 : this.state.likeCount + 1,
+    });
+
+  render() {
+    const { liked, likeCount } = this.state;
+
+    return (
+      <div css={styles.actionWrapper}>
+        <button
+          type="button"
+          css={styles.likeButton(liked)}
+          onClick={() => {
+            this.toggleLiked();
+            this.setHover(true);
+          }}
+        >
+          <LikeIcon css={styles.likeIcon(liked)} className={liked ? 'click' : undefined} />
+          {likeCount} Likes
+        </button>
+      </div>
+    );
+  }
+}
