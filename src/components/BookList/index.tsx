52dd6db87f54d609589fdbd14ba0c6c2f8c4826a
shrimp1box <shrimp1box@gmail.com>
2019-05-16 02:33:57 +0900

    리스트 선택시 도서 데이터를 setState 하기 위해 정보를 id 를 key값으로 mapping

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -8,10 +8,12 @@ export interface BookListProps {
   title: string;
   author: string;
   onClickBookList: any;
+  id: string;
 }
 export class BookList extends React.Component<BookListProps> {
   render() {
-    const { thumbnailUrl, title, author, onClickBookList } = this.props;
+    const { thumbnailUrl, title, author, onClickBookList, id } = this.props;
+
     return (
       <article css={bookListStyle.wrapper}>
         <img css={bookListStyle.thumbnail} src={thumbnailUrl} alt={`${title} thumbnail image`} />
@@ -19,7 +21,12 @@ export class BookList extends React.Component<BookListProps> {
           <p css={bookListStyle.title}>{title}</p>
           <p css={bookListStyle.author}>{author}</p>
         </div>
-        <button css={bookListStyle.fullButton} onClick={onClickBookList}>
+        <button
+          css={bookListStyle.fullButton}
+          onClick={() => {
+            onClickBookList(id);
+          }}
+        >
           {title} 선택 버튼
         </button>
       </article>
